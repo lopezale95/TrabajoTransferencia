a605c60e197bb304163fcb06354fe9f726f67e42
alejalo95 <lopez.cristian666@gmail.com>
2023-05-21 19:55:02 -0500

    commit 5
    creación de componentes-Películas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PeliculaComponent } from './pelicula/pelicula.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent, PeliculaComponent
+    AppComponent, PeliculaComponent, TestComponent
   ],
   imports: [
     BrowserModule,
